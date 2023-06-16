8c76693b3b13e2320e3a69bf55d754e9ae7f9f43
ascappa <108097502+ascappa@users.noreply.github.com>
2023-06-16 09:55:16 +0200

    Start making the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./home.module.css"
 
 export const metadata = {
-  title: "Home"
-}
+  title: "Home",
+};
 
 export default function Home() {
+  return (
+    <header className={styles.header}>
+      <Image src="/vercel.svg" width={100} height={24} alt="logo" />
+      <nav>
+        <ul>
+          <li>about</li>
+          <li>blog</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
