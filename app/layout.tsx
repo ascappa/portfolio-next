23271db8f1e106c31bf6c6e793998dac59fa49ed
ascappa <108097502+ascappa@users.noreply.github.com>
2023-06-19 16:00:00 +0200

    Add gradient text and UI improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,45 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Sphere } from "iconoir-react";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'My first nextjs project',
-  description: 'Trying out nextjs',
-}
+export const metadata: Metadata = {
+  title: "My first nextjs project",
+  description: "Trying out nextjs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const navItems = ["about", "blog", "contact"];
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex justify-center p-8`}>
+        {<div className="w-full max-w-5xl">
+          <header className="flex items-center justify-between">
+            <Sphere className="w-7 text-purple-600" />
+            <nav>
+              <ul className="flex justify-evenly gap-4">
+                {navItems.map((item) => (
+                  <Link
+                    href={`/${item}`}
+                    key={item}
+                    className="rounded bg-purple-100 p-1 px-2 font-semibold uppercase text-purple-600 transition-colors hover:bg-purple-600 hover:text-purple-100"
+                  >
+                    {item}
+                  </Link>
+                ))}
+              </ul>
+            </nav>
+          </header>
+          {children}
+        </div>}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import styles from "./home.module.css"
+import uwu from "./assets/header.svg";
+import leaf from "./leaf.jpeg";
 
 export const metadata = {
   title: "Home",
@@ -7,15 +8,34 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <header className={styles.header}>
-      <Image src="/vercel.svg" width={100} height={24} alt="logo" />
-      <nav>
-        <ul>
-          <li>about</li>
-          <li>blog</li>
-          <li>contact</li>
-        </ul>
-      </nav>
-    </header>
+    <div className="text-center">
+      <h1 className="my-12 bg-gradient-to-r from-purple-600 to-rose-600 bg-clip-text text-4xl font-extrabold uppercase text-transparent inline-block">
+        Welcome to my website!
+      </h1>
+      <p className="">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+        repudiandae beatae molestias minus minima ratione iusto recusandae, ipsa
+        quibusdam obcaecati optio veniam, dolore deserunt quia fugiat. Iusto eos
+        quam optio!
+      </p>
+      <p className="">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+        repudiandae beatae molestias minus minima ratione iusto recusandae, ipsa
+        quibusdam obcaecati optio veniam, dolore deserunt quia fugiat. Iusto eos
+        quam optio!
+      </p>
+      <p className="">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+        repudiandae beatae molestias minus minima ratione iusto recusandae, ipsa
+        quibusdam obcaecati optio veniam, dolore deserunt quia fugiat. Iusto eos
+        quam optio!
+      </p>
+      <p className="">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+        repudiandae beatae molestias minus minima ratione iusto recusandae, ipsa
+        quibusdam obcaecati optio veniam, dolore deserunt quia fugiat. Iusto eos
+        quam optio!
+      </p>
+    </div>
   );
 }
